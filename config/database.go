40686bd838b16d07d18c9cf4568b500295881d1b
cheeto <dakejiang123@163.com>
2024-03-23 18:35:41 +0900

    v(0.0.1-beta): update dockerfile

--- config/database.go
+++ config/database.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -11,7 +12,14 @@ import (
 
 func ConnectToDB() *gorm.DB {
 	var err error
-	dsn := os.Getenv("DB_DSN_PRD")
+	// dsn := os.Getenv("DB_DSN_PRD")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
 	if err != nil {
--- dockerfile
+++ dockerfile
@@ -1,7 +1,12 @@
+
 FROM golang:1.19-alpine AS builder
 WORKDIR /app
 COPY . .
 RUN go mod download
+ENV DB_HOST=ep-super-sound-a4roud6p.us-east-1.pg.koyeb.app
+ENV DB_USER=root
+ENV DB_PASSWORD=P1xV9lytwFam
+ENV DB_NAME=koyebdb
 RUN go build -o ./japanese-practice-api ./main.go
  
  
