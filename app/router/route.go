a871d6cb1b1426be41119cb0a07d869b09acaae2
cheeto <dakejiang123@163.com>
2024-06-23 17:32:23 +0900

    options router

--- app/router/route.go
+++ app/router/route.go
@@ -32,6 +32,20 @@ func Init(init *config.Initialization) *gin.Engine {
 		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
 		c.Status(200)
 	})
+
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	})
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
