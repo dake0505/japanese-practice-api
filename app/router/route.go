9c9741a554064a598c65d814ab4dc3bacec0fea0
cheeto <dakejiang123@163.com>
2024-06-23 17:25:19 +0900

    update router

--- app/router/route.go
+++ app/router/route.go
@@ -3,13 +3,35 @@ package router
 import (
 	"gin-gonic-api/app/middleware"
 	"gin-gonic-api/config"
+	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Init(init *config.Initialization) *gin.Engine {
 
 	router := gin.New()
+
+	r := gin.Default()
+	// r.Use(cors.Default())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://japanese-practice-h5.vercel.app"}, // 去掉末尾的斜杠
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	r.OPTIONS("/*path", func(c *gin.Context) {
+		log.Println("Received OPTIONS request")
+		c.Header("Access-Control-Allow-Origin", "https://japanese-practice-h5.vercel.app")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+		c.Status(200)
+	})
 	router.Use(gin.Logger())
 	router.Use(gin.Recovery())
 
--- main.go
+++ main.go
@@ -3,11 +3,8 @@ package main
 import (
 	"gin-gonic-api/app/router"
 	"gin-gonic-api/config"
-	"log"
 	"os"
-	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -19,25 +16,6 @@ func init() {
 
 func main() {
 	port := os.Getenv("PORT")
-
-	r := gin.Default()
-	// r.Use(cors.Default())
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://japanese-practice-h5.vercel.app"}, // 去掉末尾的斜杠
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
-
-	r.OPTIONS("/*path", func(c *gin.Context) {
-		log.Println("Received OPTIONS request")
-		c.Header("Access-Control-Allow-Origin", "https://japanese-practice-h5.vercel.app")
-		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
-		c.Status(200)
-	})
 
 	init := config.Init(&gin.Context{})
 	app := router.Init(init)
